a1882c510362da8e14085da7d8476824fc99fc72
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2022-10-14 03:40:02 +0100

    RedisCache hostname and keys added

--- src/scc/cache/RedisCache.java
+++ src/scc/cache/RedisCache.java
@@ -4,25 +4,27 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisCache {
-	private static final String RedisHostname = System.getenv("REDIS_URL");
-	private static final String RedisKey = System.getenv("REDIS_KEY");
+	
+	private static final String REDIS_HOSTNAME = "scc56773.redis.cache.windows.net";
+	private static final String REDIS_KEY = "KAvpM7YzqoxVHFfz28hkuPvYsdSivDNRrAzCaGmhREs=";
 	
 	private static JedisPool instance;
 	
-	public synchronized static JedisPool getCachePool() {
-		if( instance != null)
-			return instance;
-		final JedisPoolConfig poolConfig = new JedisPoolConfig();
-		poolConfig.setMaxTotal(128);
-		poolConfig.setMaxIdle(128);
-		poolConfig.setMinIdle(16);
-		poolConfig.setTestOnBorrow(true);
-		poolConfig.setTestOnReturn(true);
-		poolConfig.setTestWhileIdle(true);
-		poolConfig.setNumTestsPerEvictionRun(3);
-		poolConfig.setBlockWhenExhausted(true);
-		instance = new JedisPool(poolConfig, RedisHostname, 6380, 1000, RedisKey, true);
-		return instance;
+	public static synchronized JedisPool getCachePool() {
+		if (instance == null) {
+			final JedisPoolConfig poolConfig = new JedisPoolConfig();
+			poolConfig.setMaxTotal(128);
+			poolConfig.setMaxIdle(128);
+			poolConfig.setMinIdle(16);
+			poolConfig.setTestOnBorrow(true);
+			poolConfig.setTestOnReturn(true);
+			poolConfig.setTestWhileIdle(true);
+			poolConfig.setNumTestsPerEvictionRun(3);
+			poolConfig.setBlockWhenExhausted(true);
+			instance = new JedisPool(poolConfig, REDIS_HOSTNAME, 6380, 1000, REDIS_KEY, true);
+		}
 		
+		return instance;
 	}
+	
 }
