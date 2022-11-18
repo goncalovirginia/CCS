ee67502734bdc00f8e613e422855c9d773ed5575
Fontinha <39402361+FoxJunior@users.noreply.github.com>
2022-11-18 22:33:05 +0000

    Auction / Bids / Questions Validations

--- auction.project/src/scc/srv/AuctionResource.java
+++ auction.project/src/scc/srv/AuctionResource.java
@@ -8,6 +8,7 @@ import scc.cache.RedisLayer;
 import scc.data.*;
 import jakarta.ws.rs.core.Cookie;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Path("/auction")
@@ -24,14 +25,15 @@ public class AuctionResource extends AccessControl{
 	@Produces(MediaType.APPLICATION_JSON)
 	public Auction createAuction(@CookieParam("scc:session") Cookie session, Auction auction) {
 		try {
+			validateAuction(auction);
 			checkSessionCookie(session, auction.getOwner());
 			resourceContext.getResource(UserResource.class).getUser(auction.getOwner());
 			resourceContext.getResource(MediaResource.class).fileExists(auction.getPhotoId());
 			Auction dbAuction = new Auction(db.putAuction(new AuctionDAO(auction)).getItem());
 			return RedisLayer.putAuction(dbAuction);
-		} catch( WebApplicationException e) {
+		} catch(WebApplicationException e) {
 			throw e;
-		} catch( Exception e) {
+		} catch(Exception e) {
 			throw new InternalServerErrorException( e);
 		}
 	}
@@ -63,6 +65,7 @@ public class AuctionResource extends AccessControl{
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Bid bid(@CookieParam("scc:session") Cookie session, @PathParam("id") String id, Bid bid) {
+		validateBid(bid);
 		checkSessionCookie(session, bid.getUser());
 		getAuction(id);
 		resourceContext.getResource(UserResource.class).getUser(bid.getUser());
@@ -82,6 +85,7 @@ public class AuctionResource extends AccessControl{
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Question postQuestion(@CookieParam("scc:session") Cookie session, @PathParam("id") String id, Question question) {
+		validateQuestion(question);
 		checkSessionCookie(session, question.getUser());
 		getAuction(id);
 		resourceContext.getResource(UserResource.class).getUser(question.getUser());
@@ -94,6 +98,82 @@ public class AuctionResource extends AccessControl{
 	public List<Question> listQuestions(@PathParam("id") String id) {
 		getAuction(id);
 		return db.getQuestions(id).stream().map(Question::new).toList();
+	}
+
+
+	public void validateAuction(Auction auction){
+
+		List<String> list = Arrays.asList(new String[]{"", null, "closed", "open"});
+
+		if(list.contains(auction.getId())){
+			throw new IllegalArgumentException("Auction id must not be empty!");
+		}
+
+		if(list.contains(auction.getTitle())){
+			throw new IllegalArgumentException("Auction title must not be empty!");
+		}
+
+		if(list.contains(auction.getPhotoId())){
+			throw new IllegalArgumentException("Auction photo must not be empty!");
+		}
+
+		if(list.contains(auction.getEndTime())){
+			throw new IllegalArgumentException("Auction end time must not be empty!");
+		}
+
+		if(!list.contains(auction.getStatus()) && !list.contains(auction.getStatus())){
+			throw new IllegalArgumentException("Auction status must be 'open' or 'closed'");
+		}
+		
+		if(auction.getMinPrice() < 1){
+			throw new IllegalArgumentException("Auction minimum price must not be lower than 1!");
+		}
+
+	}
+	
+	public void validateBid(Bid bid){
+
+		List<String> list = Arrays.asList(new String[]{"", null});
+
+		if(list.contains(bid.getId())){
+			throw new IllegalArgumentException("Bid id must not be empty!");
+		}
+
+		if(list.contains(bid.getAuction())){
+			throw new IllegalArgumentException("Bid action name must not be empty!");
+		}
+
+		if(list.contains(bid.getUser())){
+			throw new IllegalArgumentException("Bid username must not be empty!");
+		}
+
+		if(bid.getAmount() < 1){
+			throw new IllegalArgumentException("Bid amount must not be lower than 1!");
+		}
+
+	}
+
+	
+	public void validateQuestion(Question question){
+
+		List<String> list = Arrays.asList(new String[]{"", null});
+
+		if(list.contains(question.getId())){
+			throw new IllegalArgumentException("Question id must not be empty!");
+		}
+
+		if(list.contains(question.getAuction())){
+			throw new IllegalArgumentException("Question action name must not be empty!");
+		}
+
+		if(list.contains(question.getUser())){
+			throw new IllegalArgumentException("Question username must not be empty!");
+		}
+
+		if(list.contains(question.getText())){
+			throw new IllegalArgumentException("Question text must not be empty!");
+		}
+
 	}
 	
 }
