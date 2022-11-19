8d9953061604c6ed8e3667c75e6f1ab5f8d6d576
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2022-11-19 23:13:55 +0000

    replaced javax with jakarta implementations in AccessControl

--- auction.project/src/scc/srv/AccessControl.java
+++ auction.project/src/scc/srv/AccessControl.java
@@ -7,12 +7,12 @@ import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Cookie;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.NewCookie;
 import scc.cache.RedisLayer;
 import scc.data.*;
 import scc.utils.Hash;
 
-import javax.ws.rs.core.NewCookie;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response;
 import java.util.UUID;
 
 @Path("/user")
@@ -34,9 +34,18 @@ public class AccessControl {
 		}
 		
 		String uuid = UUID.randomUUID().toString();
-		NewCookie cookie = new NewCookie("scc:session", uuid, "/", null, 1, "sessionid", 3600, false);
+		NewCookie cookie = new NewCookie.Builder("scc:session")
+				.value(uuid)
+				.path("/")
+				.comment("sessionid")
+				.maxAge(3600)
+				.secure(false)
+				.httpOnly(true)
+				.build();
+		
 		Session s = new Session(uuid, login.userId());
 		boolean check = RedisLayer.putSession(s);
+		
 		if(!check)
 			CosmosDBLayer.getInstance().putSession(new SessionDAO(s));
 
